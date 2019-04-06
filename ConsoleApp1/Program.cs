95a7a2ac6c8748e6942d7ed8c7a9929dc7d16fe6
Debjit <debjitchatterjee31@gmail.com>
2019-04-06 20:22:00 +0530

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = 5;
+            var n2 = 15;
+            var sum = n1 + n2;
+            Console.WriteLine("The Sum is :- ");
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
